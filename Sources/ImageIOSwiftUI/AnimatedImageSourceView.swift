3c8e58e5ededd4555ea25daf6fbcfac8f26ce708
David Beck <code@davidbeck.co>
2019-07-11 07:58:14 -0700

    Use preferredFramesPerSecond with DisplayLink in SwiftUI

--- Sources/ImageIOSwiftUI/AnimatedImageSourceView.swift
+++ Sources/ImageIOSwiftUI/AnimatedImageSourceView.swift
@@ -4,13 +4,17 @@ import SwiftUI
 struct AnimatedImageSourceView: View {
 	@ObjectBinding var imageSource: ImageSource
 	@State var displayLink = DisplayLink()
-	@State var startTimestamp: TimeInterval?
+	@State var startTimestamp: TimeInterval? = .none
 	@State var animationFrame: Int = 0
 	var label: Text
 	
+	init(imageSource: ImageSource, label: Text) {
+		self.imageSource = imageSource
+		self.label = label
+		self.$$displayLink = State(initialValue: DisplayLink(preferredFramesPerSecond: imageSource.preferredFramesPerSecond))
+	}
+	
 	var body: some View {
-		//		displayLink.preferredFramesPerSecond = imageSource.preferredFramesPerSecond
-		
 		return StaticImageSourceView(imageSource: imageSource, animationFrame: self.animationFrame, label: label)
 			.onAppear {
 				self.startTimestamp = CACurrentMediaTime()
--- Sources/ImageIOSwiftUI/DisplayLink.swift
+++ Sources/ImageIOSwiftUI/DisplayLink.swift
@@ -2,38 +2,50 @@ import Combine
 import Foundation
 import QuartzCore
 
-public struct DisplayLink: Publisher {
+struct DisplayLink: Publisher {
 	class Subscription<S>: Combine.Subscription where S: Subscriber, Never == S.Failure, CFTimeInterval == S.Input {
-		//		#if os(iOS)
-		//		private lazy var link = CADisplayLink(target: self, selector: #selector(displayLinkFired))
-		//		#endif
-		private var timer: Timer?
+		#if os(iOS)
+			private lazy var link = CADisplayLink(target: self, selector: #selector(displayLinkFired))
+		#else
+			private var timeInterval: TimeInterval
+			private var timer: Timer? {
+				didSet {
+					oldValue?.invalidate()
+				}
+			}
+		#endif
 		private let subscriber: AnySubscriber<CFTimeInterval, Never>
 		
 		private var demand: Subscribers.Demand = .unlimited {
 			didSet {
-				//				#if os(iOS)
-				//				link.isPaused = demand == .none
-				//				#endif
+				#if os(iOS)
+					self.link.isPaused = self.demand == .none
+				#else
+					if self.demand == .none {
+						self.timer = nil
+					} else if self.timer == nil {
+						self.timer = Timer.scheduledTimer(timeInterval: self.timeInterval, target: self, selector: #selector(self.timerFired), userInfo: nil, repeats: true)
+					}
+				#endif
 			}
 		}
 		
 		fileprivate init(subscriber: S, preferredFramesPerSecond: Int) {
 			self.subscriber = AnySubscriber(subscriber)
-			//			#if os(iOS)
-			//			link.preferredFramesPerSecond = preferredFramesPerSecond
-			//			link.add(to: .main, forMode: .default)
-			//			#endif
-			
-			let timeInterval = preferredFramesPerSecond == 0 ? 1 / 60 : 1 / TimeInterval(preferredFramesPerSecond)
-			self.timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(self.timerFired), userInfo: nil, repeats: true)
+			#if os(iOS)
+				self.link.preferredFramesPerSecond = preferredFramesPerSecond
+				self.link.add(to: .main, forMode: .default)
+			#else
+				self.timeInterval = preferredFramesPerSecond == 0 ? 1 / 60 : 1 / TimeInterval(preferredFramesPerSecond)
+			#endif
 		}
 		
 		deinit {
-			//			#if os(iOS)
-			//			link.invalidate()
-			//			#endif
-			timer?.invalidate()
+			#if os(iOS)
+				link.invalidate()
+			#else
+				timer?.invalidate()
+			#endif
 		}
 		
 		func request(_ demand: Subscribers.Demand) {
@@ -44,21 +56,27 @@ public struct DisplayLink: Publisher {
 			self.demand = .none
 		}
 		
-		//		#if os(iOS)
-		//		@objc private func displayLinkFired(_ link: CADisplayLink) {
-		//			guard demand != .none else { return }
-		//			// this seems to always return .max(0)
-		//			_ = subscriber.receive(self.link.targetTimestamp)
-		//		}
-		//		#endif
-		@objc private func timerFired() {
-			guard self.demand != .none else { return }
-			// this seems to always return .max(0)
-			_ = self.subscriber.receive(CACurrentMediaTime())
-		}
+		#if os(iOS)
+			@objc private func displayLinkFired(_: CADisplayLink) {
+				guard self.demand != .none else { return }
+				// this seems to always return .max(0)
+				_ = self.subscriber.receive(self.link.targetTimestamp)
+			}
+			
+		#else
+			@objc private func timerFired() {
+				guard self.demand != .none else { return }
+				// this seems to always return .max(0)
+				_ = self.subscriber.receive(CACurrentMediaTime())
+			}
+		#endif
 	}
 	
-	public var preferredFramesPerSecond: Int = 0
+	public var preferredFramesPerSecond: Int
+	
+	public init(preferredFramesPerSecond: Int = 0) {
+		self.preferredFramesPerSecond = preferredFramesPerSecond
+	}
 	
 	public func receive<S>(subscriber: S) where S: Subscriber, Never == S.Failure, CFTimeInterval == S.Input {
 		let subscription = Subscription(subscriber: subscriber, preferredFramesPerSecond: self.preferredFramesPerSecond)
@@ -69,6 +87,4 @@ public struct DisplayLink: Publisher {
 	public typealias Output = CFTimeInterval
 	
 	public typealias Failure = Never
-	
-	public init() {}
 }
