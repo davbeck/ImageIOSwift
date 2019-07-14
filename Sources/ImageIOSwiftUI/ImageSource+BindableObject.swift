d5bdaf7c485781815a758a3fa863a49fc9299389
David Beck <code@davidbeck.co>
2019-07-14 11:32:13 -0700

    Fix remote loading

--- Sources/ImageIOSwiftUI/ImageSource+BindableObject.swift
+++ Sources/ImageIOSwiftUI/ImageSource+BindableObject.swift
@@ -4,9 +4,7 @@ import SwiftUI
 
 @available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension ImageSource: BindableObject {
-	public var didChange: AnyPublisher<ImageSource, Never> {
+	public var didChange: NotificationCenter.Publisher {
 		return NotificationCenter.default.publisher(for: ImageSource.didUpdateData, object: self)
-			.compactMap { $0.object as? ImageSource }
-			.eraseToAnyPublisher()
 	}
 }
